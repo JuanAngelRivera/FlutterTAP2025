e05ce5e3503eb7abbda507fd576b1ae3420c76f1
Cherrisita <juangelrt21@gmail.com>
2025-04-10 10:51:04 -0600

    JuiceEntities añadidas al Screen, falta añadir el contructor

--- lib/screens/challenge_screen.dart
+++ lib/screens/challenge_screen.dart
@@ -1,38 +1,161 @@
 import 'package:flutter/material.dart';
 
-class ChallengeScreen extends StatelessWidget {
+class JuiceWidget extends StatelessWidget {
+  JuiceWidget(JuiceEntity juiceList);
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Flutter Challenge")),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: JuiceWidget(),
-        ),
+    final textStyle = TextStyle(
+      color: Colors.white,
+      fontSize: 16,
+      fontWeight: FontWeight.w600,
+      fontFamily: "Arial",
+    );
+    return AspectRatio(
+      aspectRatio: 1.25,
+      child: LayoutBuilder(
+        builder: (context, constraints) {
+          final topPadding = constraints.maxHeight * 0.2;
+          final leftPadding = constraints.maxWidth * 0.1;
+          final imageWidth = constraints.maxWidth * 0.35;
+          return Stack(
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: topPadding),
+                decoration: BoxDecoration(
+                  color: Color(0xFFDC691F),
+                  borderRadius: BorderRadius.circular(24),
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.only(
+                        top: topPadding,
+                        left: leftPadding,
+                      ),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Juguito de naranja",
+                            style: textStyle.copyWith(fontSize: 20),
+                          ),
+                          RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: "\$",
+                                  style: textStyle.copyWith(fontSize: 16),
+                                ),
+                                TextSpan(
+                                  text: ("10 pesitos"),
+                                  style: textStyle.copyWith(
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w800,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 32,
+                            width: 80,
+                            child: TextButton(
+                              style: TextButton.styleFrom(
+                                backgroundColor: Colors.white,
+                                foregroundColor: Color(0xFFDC691F),
+                                textStyle: TextStyle(
+                                  fontFamily: "Arial",
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: Text("nya"),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: imageWidth,
+                    child: Image.asset('assets/juice.png'),
+                  ),
+                ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
 }
 
-class JuiceWidget extends StatelessWidget {
+class JuiceEntity {
+  final String name;
+  final String image;
+  final String price;
+  final Color color;
+
+  JuiceEntity({
+    required this.name,
+    required this.image,
+    required this.price,
+    required this.color,
+  });
+}
+
+class ChallengeScreen extends StatelessWidget {
+  final juiceList = [
+    JuiceEntity(
+      name: "Juguito de limon",
+      image: "assets/juice1.png",
+      price: "15 pesitos",
+      color: Color(0xFFF3BE39),
+    ),
+    JuiceEntity(
+      name: "Juguito de naranja",
+      image: "assets/juice2.png",
+      price: "10 pesitos",
+      color: Color(0xFFDC691F),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 1.25,
-      child: Stack(
+    return Scaffold(
+      body: Column(
         children: [
           Container(
-            decoration: BoxDecoration(
-              color: Color(0xFFDC691F),
-              borderRadius: BorderRadius.circular(24),
+            height: 38,
+            margin: EdgeInsets.only(left: 20, top: 32, right: 20, bottom: 8),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Image.asset("assets/menu.png"),
+                Text(
+                  "Cherry´s Cafe",
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.w800),
+                ),
+                Image.asset("assets/bag.png"),
+              ],
             ),
           ),
-          Row(
-            children: [
-              Expanded(child: Placeholder()),
-              SizedBox(width: 120, child: Image.asset('assets/juice.png')),
-            ],
+          Expanded(
+            child: Center(
+              child: ListView.builder(
+                padding: EdgeInsets.all(20),
+                itemBuilder: (context, index) {
+                  return JuiceWidget(juiceList[index]);
+                },
+                itemCount: juiceList.length,
+              ),
+            ),
           ),
         ],
       ),
